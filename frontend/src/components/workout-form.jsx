130838299c1578764541f4a400b1339a6fd388f7
Emre Kılıç <emre1kilic@outlook.com>
2023-05-06 13:26:15 +0300

    add create workout form

---
+++ frontend/src/components/workout-form.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+function WorkoutForm(params) {
+  const [title, setTitle] = useState("");
+  const [reps, setReps] = useState("");
+  const [load, setLoad] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const workout = { title, reps, load };
+
+    const response = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const resData = await response.json();
+
+    if (!response.ok) {
+      setError(resData.error);
+    }
+    if (response.ok) {
+      setTitle("");
+      setReps("");
+      setLoad("");
+      setError(null);
+      console.log("Workout created successfully!", resData);
+    }
+  };
+
+  return (
+    <>
+      <div className="block mx-auto max-w-xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Create Workout
+        </h5>
+        <form onSubmit={handleSubmit}>
+          <div className="container">
+            <label>Workout Title</label>
+            <input
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div className="container">
+            <label>Workout Repeats</label>
+            <input
+              type="number"
+              onChange={(e) => setReps(e.target.value)}
+              value={reps}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div className="container">
+            <label>Workout Load</label>
+            <input
+              type="number"
+              onChange={(e) => setLoad(e.target.value)}
+              value={load}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center bg-blue-600 rounded-md text-white"
+          >
+            Add Workout
+          </button>
+          {error && <div className="text-red-500">{error}</div>}
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default WorkoutForm;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import WorkoutCard from "../components/workout-card";
+import WorkoutForm from "../components/workout-form";
 
 function Home(params) {
   const [workouts, setWorkouts] = useState([]); // [state, setState
@@ -20,20 +21,29 @@ function Home(params) {
     <>
       <div id="home">
         <h1 className="text-3xl ">Home</h1>
-        <div id="workouts">
-          {workouts.map((workout) => {
-            return (
-              <>
-                <WorkoutCard
-                  key={workout._id}
-                  title={workout.title}
-                  reps={workout.reps}
-                  load={workout.load}
-                  createdAt={workout.createdAt}
-                />
-              </>
-            );
-          })}
+        <div id="workout-form">
+          <WorkoutForm />
+        </div>
+
+        <div id="workouts" className="container mx-auto  mt-6">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            My Workouts
+          </h5>
+          <div className="grid grid-cols-4 gap-4">
+            {workouts.map((workout) => {
+              return (
+                <>
+                  <WorkoutCard
+                    key={workout._id}
+                    title={workout.title}
+                    reps={workout.reps}
+                    load={workout.load}
+                    createdAt={workout.createdAt}
+                  />
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
     </>
